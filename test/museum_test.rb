2993e4b6cd57e6f2b03512472eadf9a6972806b5
Alex Schwartz <aschwartz@detaso.com>
2021-01-07 10:34:49 -0600

    Begin test for adding exhibits

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -1,10 +1,22 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/museum'
+require './lib/exhibit'
 
 class MuseumTest < Minitest::Test
   def setup
     @moma = Museum.new('Museum of Modern Art')
+  end
+
+  def exhibits
+    {
+      chemex: create_exhibit({name: "Chemex", cost: 0}),
+      rocks: create_exhibit({name: "Rocks", cost: 0})
+    }
+  end
+
+  def create_exhibit(data)
+    Exhibit.new(data)
   end
 
   def test_it_exists
@@ -14,5 +26,12 @@ class MuseumTest < Minitest::Test
   def test_readable_attributes
     assert_equal 'Museum of Modern Art', @moma.name
     assert_equal [], @moma.exhibits
+  end
+
+  def test_can_add_exhibits
+    @moma.add_exhibit(exhibits[:chemex])
+
+    assert_equal 1, @moma.exhibits.length
+    assert_equal "Chemex", @moma.exhibits.first.name
   end
 end
