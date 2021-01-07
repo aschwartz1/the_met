867d29f89f3f87c2926387230325459604cff818
Alex Schwartz <aschwartz@detaso.com>
2021-01-07 10:05:24 -0600

    Add instance test

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -2,4 +2,9 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class ExhibitTest < Minitest::Test
+  def test_it_exists
+    exhibit = Exhibit.new
+
+    assert_instance_of Exhibit, exhibit
+  end
 end
